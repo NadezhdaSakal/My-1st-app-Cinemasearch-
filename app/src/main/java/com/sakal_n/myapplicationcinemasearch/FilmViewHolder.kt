4ecce70767d6e803e493e95f50f245f33a5993f7
NadezhdaSakal <79761587+NadezhdaSakal@users.noreply.github.com>
2021-10-25 16:09:52 +0300

    17 add RatingDonutView

--- app/src/main/java/com/sakal_n/myapplicationcinemasearch/FilmViewHolder.kt
+++ app/src/main/java/com/sakal_n/myapplicationcinemasearch/FilmViewHolder.kt
@@ -10,6 +10,8 @@ class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val title = itemView.title
     private val poster = itemView.poster
     private val description = itemView.description
+    private val ratingDonut = itemView.rating_donut
+
 
     fun bind(film: Film) {
         title.text = film.title
@@ -21,6 +23,8 @@ class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             //Указываем ImageView, куда будем загружать изображение
             .into(poster)
         description.text = film.description
+        ratingDonut.setProgress((film.rating * 10).toInt())
+
     }
 
 }
